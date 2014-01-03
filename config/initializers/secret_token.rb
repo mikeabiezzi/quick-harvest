8df9709252526963f5f0b220d87c85004ed9f7e0
Mike AbiEzzi <mjezzi@gmail.com>
2014-01-03 10:12:16 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QuickHarvest::Application.config.secret_key_base = 'dff075eec7ba6c39399ad069cc653db94b3015fa58f4fc8154af5d9c8f8d550cbe8d706dd35b2bbc1cff9967b5a0ee1788c8aa1bd9da894a938284bbcb7c8f56'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+QuickHarvest::Application.config.session_store :cookie_store, key: '_quick-harvest_session'
